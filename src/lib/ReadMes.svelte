b85d7eff736e480e1e3550e6784f8e1d3abb03d1
FomasTreeman <tom@team-freeman.com>
2023-07-20 00:01:32 +0100

    animations on scroll

--- src/lib/ReadMes.svelte
+++ src/lib/ReadMes.svelte
@@ -1,19 +1,21 @@
 <script>
-  import showdown from "showdown";
-  import { onMount } from "svelte";
-  import { Base64 } from "../../node_modules/js-base64";
-  import ReadMeTitle from "./ReadMeTitle.svelte";
+  import showdown from 'showdown';
+  import { onMount } from 'svelte';
+  import { Base64 } from '../../node_modules/js-base64';
+  import ReadMeTitle from './ReadMeTitle.svelte';
+  import Saos from 'saos';
+
   let repos = [];
   const productions = {
-    Comms: "https://comm-a.vercel.app/",
-    "Weather-TM": "https://fac27.github.io/Weather-TM/",
+    Comms: 'https://comm-a.vercel.app/',
+    'Weather-TM': 'https://fac27.github.io/Weather-TM/',
   };
 
   const pinned = [
-    "Comms",
-    "Weather-TM",
+    'Comms',
+    'Weather-TM',
     // "SoonTHC",
-    "KodiTV",
+    'KodiTV',
     // "Three.js-Experiment",
     // "face-recognition-brain",
     // "Boids_Development",
@@ -32,13 +34,13 @@
       `https://api.github.com/users/FomasTreeman/repos?per_page=100`,
       {
         headers: {
-          Accept: "application/vnd.github+json",
+          Accept: 'application/vnd.github+json',
           Authorization: import.meta.env.TOKEN,
         },
       }
     );
     const jsonResp = await resp.json();
-    const repoNames = jsonResp.map((repo) => repo.full_name.split("/")[1]);
+    const repoNames = jsonResp.map((repo) => repo.full_name.split('/')[1]);
     const pinnedRepos = repoNames.filter((name) => pinned.includes(name));
     await Promise.all(
       pinnedRepos.map(async (repoName) => {
@@ -61,35 +63,44 @@
 
     return reducedHtml;
   }
+
+  function handleObserver(x) {
+    console.info(x.detail.observing);
+  }
 </script>
 
 <!-- .md file -->
 {#each repos as repo, index}
   <!-- <div class={index % 2 == 0 ? "flip" : ""}> -->
-  <div>
-    <!-- <img src="{repo.name}.png" alt="project scrnsht" /> -->
-    <article id="md ">
-      {#await promise}
-        <h1>... waiting</h1>
-      {:then converter}
-        <header>
-          {#if productions[repo.name]}
-            <a href={productions[repo.name]} class="prod-link">
+  <Saos
+    animation={'slide-in-bottom 1s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}
+  >
+    <div>
+      <!-- <img src="{repo.name}.png" alt="project scrnsht" /> -->
+      <article id="md ">
+        {#await promise}
+          <h1>... waiting</h1>
+        {:then converter}
+          <header>
+            {#if productions[repo.name]}
+              <a href={productions[repo.name]} class="prod-link">
+                <ReadMeTitle {repo} />
+              </a>
+            {:else}
               <ReadMeTitle {repo} />
-            </a>
-          {:else}
-            <ReadMeTitle {repo} />
-          {/if}
-        </header>
-        {@html renderHtml(converter.makeHtml(repo.md))}
-        <button>...</button>
-        <!-- {@html converter.makeHtml(repo.md)} -->
-      {:catch}
-        <h1>page in development</h1>
-      {/await}
-    </article>
-  </div>
+            {/if}
+          </header>
+          {@html renderHtml(converter.makeHtml(repo.md))}
+          <button>...</button>
+          <!-- {@html converter.makeHtml(repo.md)} -->
+        {:catch}
+          <h1>page in development</h1>
+        {/await}
+      </article>
+    </div>
+  </Saos>
 {/each}
+<Saos on:update={handleObserver}>...</Saos>
 
 <style>
   article {
@@ -125,6 +136,31 @@
 
   a.prod-link:hover {
     mix-blend-mode: lighten;
+  }
+
+  @-webkit-keyframes -global-slide-in-bottom {
+    0% {
+      -webkit-transform: translateY(1000px);
+      transform: translateY(1000px);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+  @keyframes -global-slide-in-bottom {
+    0% {
+      -webkit-transform: translateY(1000px);
+      transform: translateY(1000px);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+      opacity: 1;
+    }
   }
 
   @media (max-width: 768px) {
